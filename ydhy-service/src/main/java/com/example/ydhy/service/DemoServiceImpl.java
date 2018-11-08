412cabb75d0d32ba0cd2dc8a68bf16b958babc98
傅郑锋 <fuzhengfeng@zhuojianchina.com>
2018-11-08 17:29:02 +0800

    第二次提交

--- ydhy-service/src/main/java/com/example/ydhy/service/DemoServiceImpl.java
+++ ydhy-service/src/main/java/com/example/ydhy/service/DemoServiceImpl.java
@@ -4,6 +4,9 @@ import com.example.ydhy.dao.UserMapper;
 import com.example.ydhy.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import tk.mybatis.mapper.entity.Example;
+
+import java.util.List;
 
 @Service("demoService")
 public class DemoServiceImpl implements DemoService {
@@ -11,7 +14,14 @@ public class DemoServiceImpl implements DemoService {
     private UserMapper userMapper;
     @Override
     public User getSomething() {
-        User user=userMapper.selectByPrimaryKey(1);
-        return user;
+
+        Example e=new Example(User.class);
+        e.createCriteria().getAllCriteria();
+        List<User> user=userMapper.selectByExample(e);
+        User user2=new User();
+        user2.setId(1);
+        User user1=userMapper.selectByPrimaryKey(user2);
+        return user.get(0);
+
     }
 }
--- ydhy-web/src/main/java/com/example/ydhy/YdhyWebApplication.java
+++ ydhy-web/src/main/java/com/example/ydhy/YdhyWebApplication.java
@@ -1,15 +1,18 @@
 package com.example.ydhy;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @EnableAutoConfiguration
 @MapperScan("com.example.ydhy.dao")
+@EnableSwagger2
+@ComponentScan
 public class YdhyWebApplication {
 
     public static void main(String[] args) {
